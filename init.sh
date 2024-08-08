0bfe40988f1f58a69f2a45394b68c3df4fe474a5
Pharen <pharenchork2016@gmail.com>
2024-08-08 12:09:17 +0700

    1 push

--- init.sh
+++ init.sh
@@ -43,7 +43,7 @@ awslocal ec2 run-instances \
         # Install PM2
         npm install pm2 -g
         # Clone Node.js repository
-        git clone https://github.com/KimangKhenng/devops-ex /root/devops-ex
+        git clone https://github.com/MrPharen/devops-ex /root/devops-ex
         # Navigate to the repository and start the app with PM2
         cd /root/devops-ex
         npm install
