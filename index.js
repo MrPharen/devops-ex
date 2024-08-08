597aee9ba5a407ecfdbae8f584f08167e1df9d42
tfd-ed <info@tfdevs.com>
2024-08-08 11:18:19 +0700

    Update server2

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.status(200).send('Hello, World KKKK 陈锦鸿!');
+  res.status(200).send('Hello, World KKKK　粉にちわ!');
 });
 
 app.get('/user/:id', (req, res) => {
